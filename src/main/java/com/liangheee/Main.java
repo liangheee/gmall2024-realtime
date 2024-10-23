dbdefc039776b1bb991cbe5366348e4de3d9595c
LiangHe <heliang0126@163.com>
2024-10-23 16:34:22 +0800

    build：搭建Flink1.17电商实时数仓项目

---
+++ src/main/java/com/liangheee/Main.java
@@ -0,0 +1,11 @@
+package com.liangheee;
+
+/**
+ * @author liangheee
+ * * @date 2024/10/16
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
